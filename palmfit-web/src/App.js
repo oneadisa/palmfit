66a92b125d33fe4325b5953d81ec3735a7f263f7
Abdul Ibrahim <theoneadisa@gmail.com>
2021-10-30 23:51:36 +0100

    latest update

--- palmfit-web/src/App.js
+++ palmfit-web/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import Testimonial from './components/testimonials'
 import Footer from './components/footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
@@ -23,18 +21,7 @@ const App = () => {
 
 		
 	)
-=======
-import CalorieCalculatorModal from './components/modal/calorieCalculatorModal/calorieCalculatorModal'
-
-const App = () => {
-	return (
-	<div className="font-Nunito">
-		<h2>Welcome</h2>
-		<CalorieCalculatorModal/>
-	</div>
-		)
->>>>>>> 097a41343ca6b9d41b4bc03cbb9ebda0a167f6dc
 }
   
 
-export default App;
+export default App
--- palmfit-web/tailwind.config.js
+++ palmfit-web/tailwind.config.js
@@ -3,18 +3,23 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
-<<<<<<< HEAD
 			colors:{
 				'dark-green': 'rgba(26, 141, 141, 1)',
 				'light-gray': 'rgba(246, 2251, 251, 1)',
 				'light-purple': 'rgba(247, 248, 255, 1)',
 				'light-cream': 'rgba(255, 252, 250, 1)',
 				teal:'#1A8D8D',
-				background:'#F6FBFB'
+				background:'#F6FBFB',
+				'teal-bg': '#1A8D8D',
+				'gray-bg': '#F9F9F9',
+				'gray-text': '#E8F1E8',
+				'gray-text-2': '#E8F1E8',
+				'black-text': '#1B381A',
 			},
 			fontFamily:{
 				body: 'Nunito-Sans', 
-				head: 'PT-Serif'
+				head: 'PT-Serif',	
+				Nunito: ['Nunito Sans'],
 			}
 		},
 		container: {
@@ -26,20 +31,9 @@ module.exports = {
 				'2xl': "1124px"
 			}
 		}
-=======
-			colors: {
-				'teal-bg': '#1A8D8D',
-				'gray-bg': '#F9F9F9',
-				'gray-text': '#E8F1E8',
-				'gray-text-2': '#E8F1E8',
-				'black-text': '#1B381A',
-			},
+	
 		},
-		fontFamily: {
-			Nunito: ['Nunito Sans'],
-		},
->>>>>>> 097a41343ca6b9d41b4bc03cbb9ebda0a167f6dc
-	},
+	
 	variants: {
 		extend: {},
 	},
